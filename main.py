f39e235a8fae0fb72007ba26497ea48419dff984
martin <martinardiles.a@gmail.com>
2026-05-23 01:41:25 -0400

    se agregan comentarios de maqueteo de funciones

---
+++ main.py
@@ -0,0 +1,69 @@
+import colegio as Colegio
+import administrador as Administador
+import alumno as Alumno
+
+#Aqui va la funcion para mostrar el panel principal
+
+
+
+
+#Aqui termina la funcion para mostrar el panel principal
+
+
+#Aqui va la funcion para validacion_clave()
+#Debe permitir validar la identidad tanto al Alumno como al Administrador
+#Debe permitir solo 3 intentos
+
+
+
+
+#Aqui termina la funcion de validacion_clave()
+
+
+#Aqui va la funcion de panel_admin()
+
+
+
+
+#Aqui termina la funcion de panel_admin()
+
+
+#Aqui va la funcion de agregar_alumno()
+
+
+
+
+#Aqui termina la funcion de agregar_alumno()
+
+
+#Aqui va la funcion de panel_alumno()
+
+
+
+
+#Aqui termina la funcion de panel_alumno()
+
+
+#Aqui va la funcion de actualizar_credencial()validacion_clave
+#Debe pedir la contraseña dos v
+
+
+
+
+#Aqui termina la funcion de actualizar_credencial()
+
+
+#Aqui va la funcion de obtener_ficha()
+
+
+
+
+#Aqui termina la funcion de obtener_ficha()
+
+
+#Aqui va la funcion de generar_certificado()
+
+
+
+
+#Aqui termina la funcion de generar_certificado()
