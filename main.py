ec8bd8de813d309103b8472c1cb204fabbdca8f7
MusicaToledo <milenafigueredo1999@gmail.com>
2026-05-25 21:30:51 -0400

    validación administrador
    Creación del menú (validar y cambiar clave).

--- main.py
+++ main.py
@@ -13,6 +13,38 @@ import alumno as Alumno
 #Aqui va la funcion para validacion_clave()
 #Debe permitir validar la identidad tanto al Alumno como al Administrador
 #Debe permitir solo 3 intentos
+def main():
+    sistema= Administador()
+    intento = 3
+    contador = 0
+    autenticado = False
+
+    print("Validacion clave")
+    while contador < intento:
+        clave = ("Ingrese clave")
+        if sistema.clave:
+            print ("Acceso concedido")
+            autenticado = True
+            break
+        else:
+            contador +=1
+            intentos_restantes= intento - contador
+            print(f"clave incorrecta. Intento restante : { intentos_restantes}")
+    if not autenticado:
+        print("sistema bloqueado: has superado el limite de los 3 intentos")
+        return
+    while True:
+        print("Menu de clave")
+        print("1.Cambiar clave")
+        print("2.Salir")
+
+        opcion = input("Selecciona la opcion: ")
+        if opcion == "1":
+            nueva = input("Introduce clave nueva")
+        elif opcion == "2":
+            print("Cerrando sesion.")
+            break
+        else:print("Opcion inválida")
 
 
 
